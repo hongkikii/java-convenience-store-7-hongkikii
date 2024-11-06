7f760c6abb30e0ca1b56671b9119d10dcea6fe99
hongkikii <hyangmeeh@icloud.com>
2024-11-06 20:33:07 +0900

    feat(OutputView): 시작 안내 문구 출력 기능 구현

---
+++ src/main/java/store/OutputView.java
@@ -0,0 +1,9 @@
+package store;
+
+public class OutputView {
+    private static final String START_PROMPT = "안녕하세요. W편의점입니다.";
+
+    public void showStartPrompt() {
+        System.out.println(START_PROMPT);
+    }
+}
