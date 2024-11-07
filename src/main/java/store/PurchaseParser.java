c811ae7b7cbf85c6a49fb17ab6db1bf5829a8963
hongkikii <hyangmeeh@icloud.com>
2024-11-07 22:35:14 +0900

    refactor(PurchaseParser): 구매 입력 리팩토링
    메서드 분리
    네이밍 변경
    문법 간소화

--- src/main/java/store/PurchaseParser.java
+++ src/main/java/store/PurchaseParser.java
@@ -5,32 +5,45 @@ import java.util.Map;
 
 public class PurchaseParser {
     private static final String INVALID_INPUT = "[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.";
+    private static final String INPUT_PATTERN = "^\\[(?=.*-)[^\\]]*\\](,\\[(?=.*-)[^\\]]*\\])*$";
+    private static final String INPUT_NAME_PATTERN = "^[a-zA-Z가-힣]+$";
+    private static final String PRODUCT_DELIMITER = ",";
+    private static final String INFO_TYPE_DELIMITER = "-";
+    private static final int INPUT_MIN_QUANTITY = 1;
+    private static final int INPUT_MAX_QUANTITY = 1000;
 
     public Map<String, Integer> execute(String input) {
-        if(input == null || input.isBlank()) {
-            throw new IllegalArgumentException(INVALID_INPUT);
-        }
         validate(input);
         Map<String, Integer> result = new HashMap<>();
-        String[] productInfos = input.split(",");
+        String[] productInfos = input.split(PRODUCT_DELIMITER);
         for(String productInfo : productInfos) {
-            String[] info = productInfo.substring(1, productInfo.length() - 1).split("-");
-            validateProductName(info[0]);
-            validateProductQuantity(info[1]);
-            result.put(info[0], Integer.parseInt(info[1]));
+            String[] productNameAndQuantity = removeBrackets(productInfo).split(INFO_TYPE_DELIMITER);
+            validateProductNameAndQuantity(productNameAndQuantity);
+            result.put(productNameAndQuantity[0], Integer.parseInt(productNameAndQuantity[1]));
         }
         return result;
     }
 
     private void validate(String input) {
-        String inputPattern = "^\\[(?=.*-)[^\\]]*\\](,\\[(?=.*-)[^\\]]*\\])*$";
-        if (!input.matches(inputPattern)) {
+        if(input == null || input.isBlank()) {
+            throw new IllegalArgumentException(INVALID_INPUT);
+        }
+        if (!input.matches(INPUT_PATTERN)) {
             throw new IllegalArgumentException(INVALID_INPUT);
         }
     }
 
+    private String removeBrackets(String productInfo) {
+        return productInfo.substring(1, productInfo.length() - 1);
+    }
+
+    private void validateProductNameAndQuantity(String[] productNameAndQuantity) {
+        validateProductName(productNameAndQuantity[0]);
+        validateProductQuantity(productNameAndQuantity[1]);
+    }
+
     private void validateProductName(String input) {
-        if (!input.matches("^[a-zA-Z가-힣]+$")) {
+        if (!input.matches(INPUT_NAME_PATTERN)) {
             throw new IllegalArgumentException(INVALID_INPUT);
         }
     }
@@ -38,7 +51,7 @@ public class PurchaseParser {
     private void validateProductQuantity(String input) {
         try {
             int quantity = Integer.parseInt(input);
-            if(quantity <= 0 || quantity > 1000) {
+            if(quantity < INPUT_MIN_QUANTITY || quantity > INPUT_MAX_QUANTITY) {
                 throw new IllegalArgumentException(INVALID_INPUT);
             }
         } catch (NumberFormatException e) {
