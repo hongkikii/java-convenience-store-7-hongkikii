85f3dc1e5758742637528b894d95602692cc70ab
hongkikii <hyangmeeh@icloud.com>
2024-11-10 01:00:55 +0900

    feat(Application): 추가 구매 기능 구현

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -6,38 +6,59 @@ public class Application {
     public static void main(String[] args) {
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        outputView.showStartPrompt();
-
-        outputView.showStockPrompt();
         Stock stock = new Stock();
-        outputView.show(stock);
 
-        Purchase purchase = null;
-        while (purchase == null) {
-            try {
-                outputView.showPurchasePrompt();
-                String productInfo = inputView.readLine();
-                PurchaseParser purchaseParser = new PurchaseParser();
-                Map<String, Integer> purchaseProducts = purchaseParser.execute(productInfo);
-                purchase = new Purchase(inputView, stock, purchaseProducts);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                System.out.println();
+        while (true) {
+            outputView.showStartPrompt();
+            outputView.showStockPrompt();
+            outputView.show(stock);
+
+            Purchase purchase = null;
+            while (purchase == null) {
+                try {
+                    outputView.showPurchasePrompt();
+                    String productInfo = inputView.readLine();
+                    PurchaseParser purchaseParser = new PurchaseParser();
+                    Map<String, Integer> purchaseProducts = purchaseParser.execute(productInfo);
+                    purchase = new Purchase(inputView, stock, purchaseProducts);
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println();
+                }
+            }
+
+            Membership membership = null;
+            while (membership == null) {
+                try {
+                    outputView.showMembershipPrompt();
+                    String answer = inputView.readLine();
+                    boolean isMembershipApplied = AnswerValidator.validate(answer);
+                    membership = new Membership(isMembershipApplied, purchase);
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                    System.out.println();
+                }
+            }
+            outputView.show(purchase.getInfo(membership));
+
+            while (true) {
+                try {
+                    outputView.showAdditionalPurchasePrompt();
+                    String answer = inputView.readLine();
+                    boolean isMorePurchased = AnswerValidator.validate(answer);
+
+                    if (isMorePurchased) {
+                        System.out.println();
+                        break;
+                    }
+                    return;
+                }
+                catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
             }
         }
-
-        Membership membership = null;
-        while (membership == null) {
-            try {
-                outputView.showMembershipPrompt();
-                String answer = inputView.readLine();
-                boolean isMembershipApplied = AnswerValidator.validate(answer);
-                membership = new Membership(isMembershipApplied, purchase);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                System.out.println();
-            }
-        }
-        outputView.show(purchase.getInfo(membership));
     }
 }
