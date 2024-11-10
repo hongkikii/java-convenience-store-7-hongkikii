bbd6286dd09f966e1022b0069e3f626120313832
hongkikii <hyangmeeh@icloud.com>
2024-11-11 08:05:06 +0900

    refactor(StockTest): 공통 로직 추출

--- src/test/java/store/inventory/StockTest.java
+++ src/test/java/store/inventory/StockTest.java
@@ -14,17 +14,17 @@ import store.inventory.promotion.PromotionType;
 
 public class StockTest {
     private Stock stock;
+    private List<Product> products;
 
     @BeforeEach
     public void setUp() {
         stock = new Stock(new ProductProcessor(new PromotionProcessor()));
+        products = stock.get();
     }
 
     @DisplayName("일반 재고를 조회할 수 있다.")
     @Test
     public void 일반_재고를_조회할_수_있다() {
-        List<Product> products = stock.get();
-
         String name = "콜라";
         int price = 1000;
         int quantity = 10;
@@ -35,14 +35,13 @@ public class StockTest {
                         && product.getPrice() == price
                         && product.getQuantity() == quantity
                         && product.getPromotionType() == promotionType);
+
         assertTrue(productExists);
     }
 
     @DisplayName("프로모션 재고를 조회할 수 있다.")
     @Test
     public void 프로모션_재고를_조회할_수_있다() {
-        List<Product> products = stock.get();
-
         // 2024년 12월 31일까지 적용되는 프로모션
         String name = "콜라";
         int price = 1000;
@@ -54,14 +53,14 @@ public class StockTest {
                         && product.getPrice() == price
                         && product.getQuantity() == quantity
                         && product.getPromotionType() == promotionType);
+
         assertTrue(productExists);
     }
 
     @DisplayName("현재 프로모션 기간이 아닌 제품의 프로모션 재고를 조회할 수 없다.")
     @Test
-    public void 현재_프로모션_기간이_아닌_제품의_프로모션_재고를_조회할_수_없단() {
-        List<Product> products = stock.get();
-
+    public void 현재_프로모션_기간이_아닌_제품의_프로모션_재고를_조회할_수_없다() {
+        // 2025년 1월부터 적용되는 프로모션
         String name = "닭가슴살";
         int price = 12000;
         int quantity = 50;
@@ -72,6 +71,7 @@ public class StockTest {
                         && product.getPrice() == price
                         && product.getQuantity() == quantity
                         && product.getPromotionType() == promotionType);
+
         assertFalse(productExists);
     }
 }
