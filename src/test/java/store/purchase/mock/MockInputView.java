3d80fd5c5ad54411b5191c64a2398fe59b3f5a45
hongkikii <hyangmeeh@icloud.com>
2024-11-11 08:22:26 +0900

    refactor(MockInputView): 상수 분리

--- src/test/java/store/purchase/mock/MockInputView.java
+++ src/test/java/store/purchase/mock/MockInputView.java
@@ -3,15 +3,17 @@ package store.purchase.mock;
 import store.view.InputView;
 
 public class MockInputView extends InputView {
-    public String answer = "Y";
+    private static final String POSITIVE_ANSWER = "Y";
+
+    public String answer = POSITIVE_ANSWER;
 
     @Override
     public boolean isPositiveToGeneral(String productName, int generalCount) {
-        return answer.equals("Y");
+        return answer.equals(POSITIVE_ANSWER);
     }
 
     @Override
     public boolean isPositiveToAdd(String productName, int freeCount) {
-        return answer.equals("Y");
+        return answer.equals(POSITIVE_ANSWER);
     }
 }
